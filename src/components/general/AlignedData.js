9aa811c9840beefee84248a94a2dcd1e524eb906
Jessy Lachapelle <jlachapelle@novomnetworks.com>
2017-02-23 19:41:58 -0500

    Update style (#43)
    * Change title sizes
    * Add spacing to aligned data

--- src/components/general/AlignedData.js
+++ src/components/general/AlignedData.js
@@ -4,8 +4,12 @@ import { Col, Row } from 'react-bootstrap';
 export default class AlignedData extends Component {
   render() {
     return (
-      <Row componentClass={this.props.componentClass || 'div'}>
-        <Col md={3}>{this.props.label}</Col>
+      <Row
+        className={this.props.className}
+        componentClass={this.props.componentClass || 'div'}
+        style={{ marginTop: 5, marginBottom: 5 }}
+      >
+        <Col md={4}>{this.props.label}</Col>
         <Col>{this.props.value}</Col>
       </Row>
     );
@@ -18,7 +22,8 @@ const propType = React.PropTypes.oneOfType([
 ]).isRequired;
 
 AlignedData.propTypes = {
+  className: React.PropTypes.string,
+  componentClass: React.PropTypes.string,
   label: propType,
   value: propType,
-  componentClass: React.PropTypes.string,
 };
