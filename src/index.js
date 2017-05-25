8ca83a041f34ff0d8a9c8a3ace3d584a1c2697eb
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2017-05-25 10:19:43 -0400

    Update styling files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
 
-import './style.css';
-import './react-bootstrap-table-all.min.css';
-
 import App from './App';
 
 moment.locale('fr');
