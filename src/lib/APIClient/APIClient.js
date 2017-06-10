ca8faeac33116779ced1a95a947378d219a7dd74
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2017-06-10 14:45:45 -0400

    Create APIClient

---
+++ src/lib/APIClient/APIClient.js
@@ -0,0 +1,160 @@
+import HTTP from './HTTP';
+
+export default class APIClient {
+  constructor(url, key) {
+    this.url = url;
+    this.key = key;
+
+    HTTP.setAPIKey(key);
+    HTTP.setAPIurl(url);
+  }
+
+  get category() {
+    return {
+      select: (callback) => {
+        HTTP.post(`${this.url}/category/select`, {}, callback);
+      },
+    };
+  }
+
+  get comment() {
+    return {
+      delete: (id, callback) => {
+        HTTP.post(`${this.url}/comment/delete`, { id }, callback);
+      },
+      insert: (member, comment, callback) => {
+        HTTP.post(`${this.url}/comment/insert`, { member, comment }, callback);
+      },
+      update: (id, comment, callback) => {
+        HTTP.post(`${this.url}/comment/update`, { id, comment }, callback);
+      },
+    };
+  }
+
+  get copy() {
+    return {
+      delete: (id, callback) => {
+        HTTP.post(`${this.url}/copy/delete`, { id }, callback);
+      },
+      insert: (memberNo, itemId, price, callback) => {
+        const data = {
+          item_id: itemId,
+          member_no: memberNo,
+          price,
+        };
+
+        HTTP.post(`${this.url}/copy/insert`, data, callback);
+      },
+      update: (id, price, callback) => {
+        HTTP.post(`${this.url}/copy/update`, { id, price }, callback);
+      },
+    };
+  }
+
+  get item() {
+    return {
+      exists: (ean13, callback) => {
+        HTTP.post(`${this.url}/item/exists`, { ean13 }, callback);
+      },
+      insert: (item, callback) => {
+        HTTP.post(`${this.url}/item/insert`, { item }, callback);
+      },
+      search: (search, options, callback) => {
+        HTTP.post(`${this.url}/item/search`, { ...options, search }, callback);
+      },
+      select: (options, callback) => {
+        HTTP.post(`${this.url}/item/select`, options, callback);
+      },
+      update: (id, item, callback) => {
+        HTTP.post(`${this.url}/item/update`, { id, item }, callback);
+      },
+      updateStatus: (id, status, callback) => {
+        HTTP.post(`${this.url}/item/updateStatus`, { id, status }, callback);
+      },
+      updateStorage: (id, storage, callback) => {
+        HTTP.post(`${this.url}/item/update_storage`, { id, storage }, callback);
+      },
+    };
+  }
+
+  get member() {
+    return {
+      exists: (no, callback) => {
+        HTTP.post(`${this.url}/member/exists`, { no }, callback);
+      },
+      getName: (no, callback) => {
+        HTTP.post(`${this.url}/member/getName`, { no }, callback);
+      },
+      insert: (member, callback) => {
+        HTTP.post(`${this.url}/member/insert`, member, callback);
+      },
+      pay: (no, callback) => {
+        HTTP.post(`${this.url}/member/pay`, { no }, callback);
+      },
+      renew: (no, callback) => {
+        HTTP.post(`${this.url}/member/renew`, { no }, callback);
+      },
+      search: (search, options, callback) => {
+        HTTP.post(`${this.url}/member/search`, { ...options, search }, callback);
+      },
+      select: (no, callback) => {
+        HTTP.post(`${this.url}/member/select`, { no }, callback);
+      },
+      update: (no, member, callback) => {
+        HTTP.post(`${this.url}/member/update`, { no, member }, callback);
+      },
+    };
+  }
+
+  get reservation() {
+    return {
+      delete: (member, item, callback) => {
+        HTTP.post(`${this.url}/reservation/delete`, { member, item }, callback);
+      },
+      deleteAll: (callback) => {
+        HTTP.post(`${this.url}/reservation/deleteAll`, {}, callback);
+      },
+      insert: (member, item, callback) => {
+        HTTP.post(`${this.url}/reservation/insert`, { member, item }, callback);
+      },
+      select: (callback) => {
+        HTTP.post(`${this.url}/reservation/select`, {}, callback);
+      },
+    };
+  }
+
+  get state() {
+    return {
+      select: (callback) => {
+        HTTP.post(`${this.url}/state/select`, {}, callback);
+      },
+    };
+  }
+
+  get storage() {
+    return {
+      delete: (callback) => {
+        HTTP.post(`${this.url}/storage/delete`, {}, callback);
+      },
+      select: (callback) => {
+        HTTP.post(`${this.url}/storage/select`, {}, callback);
+      },
+    };
+  }
+
+  get transaction() {
+    return {
+      delete: (copy, type, callback) => {
+        HTTP.post(`${this.url}/transaction/delete`, { copy, type }, callback);
+      },
+      insert: (memberNo, copyIDs, transactionType, callback) => {
+        const data = {
+          copies: copyIDs,
+          member: memberNo,
+          type: transactionType,
+        };
+        HTTP.post(`${this.url}/transaction/insert`, data, callback);
+      },
+    };
+  }
+}
