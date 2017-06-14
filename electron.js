4af73f65b30c6662e5acfa4d860cd4647c6aaaeb
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2017-06-13 21:23:10 -0400

    Successfull Linux build

---
+++ electron.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit());
+app.on('activate', () => mainWindow === null && createWindow());
