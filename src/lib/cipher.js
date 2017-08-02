1603db990bd79994e9e7362e805880e13ee0bd0f
Jessy Lachapelle <jlachapelle@novomnetworks.com>
2017-08-01 20:24:27 -0400

    Fix bug with cipher (#91)

--- src/lib/cipher.js
+++ src/lib/cipher.js
@@ -4,6 +4,8 @@ import settings from '../settings';
 
 const algorithm = 'aes-256-ctr';
 const secretKey = settings.secretKey;
-const cipher = crypto.createCipher(algorithm, secretKey);
 
-export const encrypt = text => `${cipher.update(text, 'utf8', 'hex')}${cipher.final('hex')}`;
+export const encrypt = (text) => {
+  const cipher = crypto.createCipher(algorithm, secretKey);
+  return `${cipher.update(text, 'utf8', 'hex')}${cipher.final('hex')}`;
+};
