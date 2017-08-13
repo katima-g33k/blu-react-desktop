39b5c44f51d0646b78304becaf47a6be36c226eb
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2017-08-13 13:02:36 -0400

    Add init setup

--- src/lib/cipher.js
+++ src/lib/cipher.js
@@ -1,9 +1,9 @@
 import crypto from 'crypto';
 
-import settings from '../settings';
+import settings from './Settings';
 
 const algorithm = 'aes-256-ctr';
-const secretKey = settings.secretKey;
+const { secretKey } = settings;
 
 export const encrypt = (text) => {
   const cipher = crypto.createCipher(algorithm, secretKey);
