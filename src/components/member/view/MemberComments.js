7f3990d3934674c814d3446bc85dae845c3f5d04
Jessy Lachapelle <jlachapelle@novomnetworks.com>
2017-08-29 20:54:44 -0400

    Optimize member comments (#108)

--- src/components/member/view/MemberComments.js
+++ src/components/member/view/MemberComments.js
@@ -1,37 +1,208 @@
-import React, { Component } from 'react';
-import { Button, Glyphicon } from 'react-bootstrap';
-import { I18n, Translate } from 'react-i18nify';
+import React, { Component, PropTypes } from 'react';
+import { ButtonGroup, Col, Row } from 'react-bootstrap';
+import { I18n } from 'react-i18nify';
 
-import Table from '../../general/Table';
+import API from '../../../lib/API';
+import Button from '../../general/Button';
+import CommentColumns from './CommentColumns';
+import { ConfirmModal, InformationModal, InputModal } from '../../general/modals';
+import TableLayout from '../../general/TableLayout';
 
 export default class MemberComment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeComment: null,
+      comments: props.comments,
+      error: null,
+      showModal: null,
+    };
+
+    this.columns = CommentColumns.concat([{
+      dataField: 'action',
+      label: 'Actions',
+      dataAlign: 'center',
+      width: '100px',
+      dataFormat: this.renderRowActions,
+    }]);
+
+    this.actions = [{
+      name: 'add',
+      bsStyle: 'success',
+      icon: 'plus',
+      label: 'Ajouter',
+      onClick: this.handleNewComment,
+    }];
+  }
+
+  static propTypes = {
+    comments: PropTypes.array.isRequired,
+    member: PropTypes.string,
+  }
+
+  static defaultProps = {
+    comments: [],
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    this.resetState(nextProps.comments);
+  }
+
+  resetState = (comments = this.state.comments) => {
+    this.setState({
+      activeComment: null,
+      comments,
+      error: null,
+      showModal: null,
+    });
+  }
+
+  handleCancel = () => {
+    this.resetState();
+  }
+
+  handleButtonClick = (actionData) => {
+    this.setState(actionData);
+  }
+
+  handleNewComment = () => {
+    this.setState({ showModal: 'input' });
+  }
+
+  handleDelete = () => {
+    const { id } = this.state.activeComment;
+
+    API.comment.delete(id, (error) => {
+      if (error) {
+        this.setState({ error, activeComment: null, showModal: null });
+        return;
+      }
+
+      this.setState({
+        activeComment: null,
+        comments: this.state.comments.filter(comment => comment.id !== id),
+        showModal: null,
+      });
+    });
+  }
+
+  handleInsert = (value) => {
+    API.comment.insert(this.props.member, value, (error, res) => {
+      if (error) {
+        this.setState({ error, activeComment: null, showModal: null });
+        return;
+      }
+
+      const comments = this.state.comments || [];
+
+      comments.push({
+        id: res.id,
+        updatedAt: new Date(),
+        comment: value,
+      });
+
+      this.resetState(comments);
+    });
+  }
+
+  handleSave = (event, value) => {
+    return this.state.activeComment ? this.handleUpdate(value) : this.handleInsert(value);
+  }
+
+  handleUpdate = (value) => {
+    API.comment.update(this.state.activeComment.id, value, (error) => {
+      if (error) {
+        this.setState({ error, activeComment: null, showModal: null });
+        return;
+      }
+
+      const comments = this.state.comments;
+      const currentComment = comments.find(comment => comment.id === this.state.activeComment.id);
+
+      currentComment.comment = value;
+      currentComment.updatedAt = new Date();
+
+      this.setState({
+        comments,
+        activeComment: null,
+        showModal: null,
+      });
+    });
+  }
+
+  renderModal = () => {
+    const { activeComment, error, showModal } = this.state;
+
+    if (error) {
+      return (
+        <InformationModal
+          message={error.message}
+          onClick={this.resetState}
+          title={`Erreur ${error.code}`}
+        />
+      );
+    }
+
+    switch (showModal) {
+      case 'input':
+        return (
+          <InputModal
+            message="Entrer le commentaire"
+            title={activeComment ? 'Modifier un commentaire' : 'Ajouter un commentaire'}
+            textarea
+            value={activeComment ? activeComment.comment : ''}
+            onSave={this.handleSave}
+            onCancel={this.handleCancel}
+          />
+        );
+      case 'confirm':
+        return (
+          <ConfirmModal
+            message={`Souhaitez-vous vraiment supprimer ce commentaire : "${activeComment.comment}"`}
+            title="Supprimer un commentaire"
+            onConfirm={this.handleDelete}
+            onCancel={this.handleCancel}
+            confirmationStyle="danger"
+          />
+        );
+      default:
+        return null;
+    }
+  }
+
+  renderRowActions = (actions, activeComment) => {
+    return (
+      <ButtonGroup>
+        <Button
+          actionData={{ activeComment, showModal: 'input' }}
+          glyph="pencil"
+          onClick={this.handleButtonClick}
+        />
+        <Button
+          actionData={{ activeComment, showModal: 'confirm' }}
+          bsStyle="danger"
+          glyph="trash"
+          onClick={this.handleButtonClick}
+        />
+      </ButtonGroup>
+    );
+  }
+
   render() {
     return (
-      <section>
-        <h4>
-          <Translate value="MemberView.comment.title" />
-        </h4>
-        <Button
-          bsStyle='success'
-          onClick={this.props.onAddClick}
-        >
-          <Glyphicon glyph='plus' /> {'Ajouter'}
-        </Button>
-        <Table
-          columns={this.props.columns}
-          data={this.props.data}
-          placeholder={I18n.t('MemberView.comment.none')}
-          striped
-        />
-        {this.props.modal}
-      </section>
+      <Row>
+        <Col>
+          <TableLayout
+            actions={this.actions}
+            columns={this.columns}
+            data={this.state.comments}
+            placeholder={I18n.t('MemberView.comment.none')}
+            striped
+            title={I18n.t('MemberView.comment.title')}
+          />
+        </Col>
+        {this.renderModal()}
+      </Row>
     );
   }
 }
-
-MemberComment.propTypes = {
-  columns: React.PropTypes.array,
-  data: React.PropTypes.array,
-  modal: React.PropTypes.shape(),
-  onAddClick: React.PropTypes.func,
-};
