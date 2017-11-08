c040ad14928a9852a8d5e4db5e8d95424e3daf1e
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2017-11-07 19:48:26 -0500

    Fix bug in account model

--- src/lib/models/Account.js
+++ src/lib/models/Account.js
@@ -5,26 +5,18 @@ import Copy from './Copy';
 export default class Account {
   constructor(account = {}) {
     this.registration = account.registration;
-    this.last_activity = account.last_activity;
+    this.lastActivity = account.lastActivity;
     this.comment = account.comment ? account.comment.map(comment => new Comment(comment)) : [];
     this.copies = account.copies ? account.copies.map(copy => new Copy(copy)) : [];
     this.transfers = account.transfers || [];
   }
 
   get deactivationDate() {
-    return moment(this.last_activity).add(1, 'year');
+    return moment(this.lastActivity).add(1, 'year');
   }
 
   get isActive() {
     return moment().isBefore(this.deactivationDate);
-  }
-
-  get lastActivity() {
-    return this.last_activity;
-  }
-
-  set lastActivity(lastActivity) {
-    this.last_activity = lastActivity;
   }
 
   getAddedCopies() {
